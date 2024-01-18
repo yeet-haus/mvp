2fa084cb02aa598166cc2b14b14b63ab8ea0b1de
skuhlmann <samkuhlmann@gmail.com>
2024-01-18 15:56:39 -0700

    fixes chain enforcement on launch form

--- src/pages/Launch.tsx
+++ src/pages/Launch.tsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { H2, SingleColumnLayout } from "@daohaus/ui";
 import { FormBuilder } from "@daohaus/form-builder";
 import { APP_FORM } from "../legos/forms";
 import { AppFieldLookup } from "../legos/legoConfig";
 import { useNavigate } from "react-router-dom";
 import { useDHConnect } from "@daohaus/connect";
+import { targetNetworks } from "../utils/constants";
 
 const now = () => (new Date().getTime() / 1000).toFixed();
 const later = () => (new Date().getTime() / 1000 + 2419200).toFixed();
@@ -13,14 +14,15 @@ export const Launch = () => {
   const navigate = useNavigate();
   const { chainId } = useDHConnect();
 
-  // todo: check chainId here is a valid one and pass to formbuilder
-  console.log("chainId", chainId);
+  const validNetworkId = useMemo(() => {
+    if (!chainId) return;
+    return targetNetworks[chainId];
+  }, [chainId]);
 
   const onFormComplete = (
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     result: any
   ) => {
-    console.log("result", result);
     const daoAddress = result?.data?.transaction?.daoAddress;
     navigate(`/success/${daoAddress}`);
   };
@@ -28,7 +30,7 @@ export const Launch = () => {
     <FormBuilder
       form={APP_FORM.SUMMON_YEETER}
       customFields={AppFieldLookup}
-      targetNetwork={"0xaa36a7"}
+      targetNetwork={validNetworkId && chainId}
       submitButtonText="LET'S FUUUCKING GO"
       lifeCycleFns={{
         onPollSuccess: (result) => {
